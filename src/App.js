81d9f9e0b1f48b2c6b517ad9a8ab8a3c01594218
Aparnagopal <gopal_aparna@yahoo.com>
2024-02-01 12:48:18 +0530

    Added the base code
    Base code was included in the folder from the prev repository

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://radiant-melomakarona-b6fba3.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Aparna Gopalakrishnan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Aparnagopal/Reactweek5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://wonderful-donut-1c2dcf.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
